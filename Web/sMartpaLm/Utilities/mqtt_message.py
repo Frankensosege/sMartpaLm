038ba669c888f14f6da2e3f2bd841088451337cd
Frankensosege <frnakesosege@gmail.com>
2023-06-29 14:45:00 +0900

    20230629-03

--- Web/sMartpaLm/Utilities/mqtt_message.py
+++ Web/sMartpaLm/Utilities/mqtt_message.py
@@ -337,56 +337,59 @@ class mos_subscriber:
         if not hasattr(cls, "_init"):
             print("Start create connection....")
             self.client = self.connect_mqtt()
+            print("Start create connected....")
             cls._init = True
+            self.client.subscribe("#")
+            self.client.loop_forever()
 
-    def sub_message(self):
-        print('Waiting topic.....')
-        self.client.subscribe("#")
-        self.client.loop_forever()
+    # def sub_message(self):
+    #     print('Waiting topic.....')
+    #     self.client.subscribe("#")
+    #     self.client.loop_forever()
 
     def connect_mqtt(self):
-        def connect_mqtt(self):
-            def on_connect(client, userdata, flags, rc):
-                print('on_connect called!!!!!!!!')
-                # client.subscribe(self.topic)
-                if rc == 0:
-                    print("Connected to MQTT broker OK....")
-                    self.connected_to_broker = True
-                else:
-                    print("Failed to connect, result code : ", rc)
+        def on_connect(client, userdata, flags, rc):
+            print('on_connect called!!!!!!!!')
+            # client.subscribe(self.topic)
+            if rc == 0:
+                print("Connected to MQTT broker OK....")
+                self.connected_to_broker = True
+            else:
+                print("Failed to connect, result code : ", rc)
 
-            def on_disconnect(client, userdata, flags, rc=0):
-                print("Disconnected result code : ", str(rc))
+        def on_disconnect(client, userdata, flags, rc=0):
+            print("Disconnected result code : ", str(rc))
 
-            def on_subscribe(client, userdata, mid, granted_qos):
-                print("---------------- on_subscribe")
-                print("subscribed: " + str(mid) + " " + str(granted_qos))
+        def on_subscribe(client, userdata, mid, granted_qos):
+            print("---------------- on_subscribe")
+            print("subscribed: " + str(mid) + " " + str(granted_qos))
 
-            def on_message(client, userdata, msg):
-                print("---------------- on_messge")
-                print(f"Received '{msg.payload.decode()}' from '{msg.topic}' topic")
-                post_messge(msg)
+        def on_message(client, userdata, msg):
+            print("---------------- on_messge")
+            print(f"Received '{msg.payload.decode()}' from '{msg.topic}' topic")
+            post_messge(msg)
 
-            def on_log(client, userdata, level, buf):
-                print(f"log: {buf}")
+        def on_log(client, userdata, level, buf):
+            print(f"log: {buf}")
 
-            print('connect to broker')
-            # 새로운 클라이언트 생성
-            client_id = f"mqtt_client_{random.randint(0, 1000)}"
-            client = mqtt.Client(client_id)
+        print('connect to broker')
+        # 새로운 클라이언트 생성
+        client_id = f"mqtt_client_{random.randint(0, 1000)}"
+        client = mqtt.Client(client_id)
 
-            # 콜백 함수 설정 on_connect(브로커에 접속), on_disconnect(브로커에 접속중료), on_publish(메세지 발행)
-            client.on_connect = on_connect
-            client.on_disconnect = on_disconnect
-            client.on_subscribe = on_subscribe
-            client.on_message = on_message
-            client.on_log = on_log
+        # 콜백 함수 설정 on_connect(브로커에 접속), on_disconnect(브로커에 접속중료), on_publish(메세지 발행)
+        client.on_connect = on_connect
+        client.on_disconnect = on_disconnect
+        client.on_subscribe = on_subscribe
+        client.on_message = on_message
+        client.on_log = on_log
 
-            # address : localhost, port: 1883 에 연결
-            # self.client.connect('localhost', 1883)
-            client.connect(get_property('URLs', 'mqtt_sever'), int(get_property('URLs', 'mqtt_port')), 60)
+        # address : localhost, port: 1883 에 연결
+        # self.client.connect('localhost', 1883)
+        print("----connecting-----")
+        client.connect(get_property('URLs', 'mqtt_sever'), int(get_property('URLs', 'mqtt_port')), 60)
 
-            return client
+        return client
 
         def post_messge(msg):
             image_folder = get_property('DATA', 'base_dir') + get_property('DATA', 'imgpath')
--- Web/sMartpaLm/mqtt_sub.py
+++ Web/sMartpaLm/mqtt_sub.py
@@ -2,4 +2,3 @@ from Utilities.mqtt_message import mos_subscriber
 
 if __name__=='__main__':
     mqsub = mos_subscriber()
-    mqsub.sub_message()
